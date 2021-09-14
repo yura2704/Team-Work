bd6364a9ddbb430b5cba6ec6907bef72a555eac2
Ihor Sharov <cookie50ds@gmail.com>
2021-09-14 21:25:43 +0300

    created /users route respond

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
+import fs from "fs";
 
 const PORT = 3000;
 const app = express();
 
+app.get('/users', cors(), (req, res) => {
+  fs.readFile('users.json', 'utf8', ((err, data) => {
+    if (err) res.status(404).send(err);
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify(JSON.parse(data)));
+  }))
+})
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port: ${PORT}...`);
