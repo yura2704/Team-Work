1b34cca6403ba16f768f222821e95d4be3e11b58
Ihor Sharov <cookie50ds@gmail.com>
2021-09-14 21:18:25 +0300

    created server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+// import cors from 'cors';
+
+const PORT = 3000;
+const app = express();
+
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port: ${PORT}...`);
+})
